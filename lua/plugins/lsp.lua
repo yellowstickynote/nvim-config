e8bd953cc32769d8045c9b843f2425f5736534c0
wassim <wassim_fkih_pr@proton.me>
2026-05-25 11:37:42 +0100

    Change lspconfig icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,15 @@
 return{
-    'https://github.com/neovim/nvim-lspconfig',
-    config = function ()
-        vim.diagnostic.config({
-            severity_sort = true,
-            signs = {
-                text = {
-                    [vim.diagnostic.severity.ERROR] = "",
-                    [vim.diagnostic.severity.WARN]  = "",
-                    [vim.diagnostic.severity.HINT]  = "",
-                    [vim.diagnostic.severity.INFO]  = "",
-                },
-            },
-        })
-    end
+	'https://github.com/neovim/nvim-lspconfig',
+	vim.diagnostic.config({
+		underline = false,
+		signs = {
+			active = true,
+			text = {
+				[vim.diagnostic.severity.ERROR] = "",
+				[vim.diagnostic.severity.WARN]  = "",
+				[vim.diagnostic.severity.HINT]  = "󰟃",
+				[vim.diagnostic.severity.INFO]  = "",
+			},
+		},
+	})
 }
