2d8687140a44a83ace5a01470bb466986b3c6370
wassim <wassim_fkih_pr@proton.me>
2026-05-25 12:33:59 +0100

    Add severity sort

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,14 @@
 return{
 	'https://github.com/neovim/nvim-lspconfig',
 	vim.diagnostic.config({
-		underline = false,
+		virtual_text = {
+			spacing = 4,
+			source = "if_many",
+			prefix = "●",
+			-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+			-- prefix = "icons",
+		},
+		severity_sort = true,
 		signs = {
 			active = true,
 			text = {
@@ -11,5 +18,9 @@ return{
 				[vim.diagnostic.severity.INFO]  = "",
 			},
 		},
-	})
+		inlay_hints = {
+			enabled = true,
+			exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+		},
+	}),
 }
