8caec07a01de163d99e3c7fd0b256a5e12e0f906
wassim <wassim_fkih_pr@proton.me>
2026-05-28 14:18:56 +0100

    add lsp capabilities for blink.cmp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,26 +1,44 @@
-return{
-	'https://github.com/neovim/nvim-lspconfig',
-	vim.diagnostic.config({
-		virtual_text = {
-			spacing = 4,
-			source = "if_many",
-			prefix = "●",
-			-- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-			-- prefix = "icons",
-		},
-		severity_sort = true,
-		signs = {
-			active = true,
-			text = {
-				[vim.diagnostic.severity.ERROR] = "",
-				[vim.diagnostic.severity.WARN]  = "",
-				[vim.diagnostic.severity.HINT]  = "󰟃",
-				[vim.diagnostic.severity.INFO]  = "",
+return {
+	'neovim/nvim-lspconfig',
+	-- Ensuring blink.cmp is treated as a dependency so it's available
+	dependencies = { 'saghen/blink.cmp' }, 
+	config = function()
+		-- 1. Configure Diagnostics
+		vim.diagnostic.config({
+			virtual_text = {
+				spacing = 4,
+				source = "if_many",
+				prefix = "●",
 			},
-		},
-		inlay_hints = {
-			enabled = true,
-			exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
-		},
-	}),
+			severity_sort = true,
+			signs = {
+				active = true,
+				text = {
+					[vim.diagnostic.severity.ERROR] = "",
+					[vim.diagnostic.severity.WARN]  = "",
+					[vim.diagnostic.severity.HINT]  = "󰟃",
+					[vim.diagnostic.severity.INFO]  = "",
+				},
+			},
+			inlay_hints = {
+				enabled = true,
+				exclude = { "vue" },
+			},
+		})
+
+		-- 2. Inject blink.cmp capabilities into lspconfig globally
+		-- This ensures ANY language server you setup inherits blink's capabilities
+		local lspconfig = require('lspconfig')
+		local lspconfig_defaults = lspconfig.util.default_config
+		
+		lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+			'force',
+			lspconfig_defaults.capabilities,
+			require('blink.cmp').get_lsp_capabilities()
+		)
+
+		-- 3. Setup your language servers normally below
+		-- lspconfig.ts_ls.setup({})
+		-- lspconfig.pyright.setup({})
+	end
 }
