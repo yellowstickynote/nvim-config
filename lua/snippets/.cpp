81a89f6d355905d5678767fdf366b4db823c4031
wassim <wassim_fkih_pr@proton.me>
2026-08-23 13:18:05 +0100

    Organize snippets

---
+++ lua/snippets/.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef MYPC
+#include "debug.cpp"
+#else
+#define debug(...)
+#define debugArr(...)
+#endif
+//#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+template <class T>
+using Tree =tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define int long long
+typedef pair<int, int> pint;
+#define vint vector<int>
+#define fast_cin()															 \
+	ios_base::sync_with_stdio(false);										  \
+	cin.tie(NULL);															 \
+	cout.tie(NULL)
+auto make_vector(int n, T val) {
+    return std::vector<T>(n, val);
+}
+
+template<typename... Args>
+auto make_vector(int n, Args... args) {
+    auto inner = make_vector(args...);
+    return std::vector<decltype(inner)>(n, inner);
+}
+constexpr int mod = 1e9+7;
+
+void solve() {
+
+}
+signed main() {
+	fast_cin();
+#ifdef MYPC
+	freopen("input.in", "r", stdin);
+	//freopen("input.out","w",stdout);
+#else
+	string filename="";
+	if(!filename.empty()) {
+		freopen((filename+".in").c_str(), "r", stdin);
+		freopen((filename+".out").c_str(), "w", stdout);
+	}
+#endif
+	int t;
+	t=1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
